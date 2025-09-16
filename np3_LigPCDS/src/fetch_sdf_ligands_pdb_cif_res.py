3e8af43626379231d644349995d60da813a709e8
crisfbazz <crisfbazz@gmail.com>
2025-09-16 19:10:34 -0300

    renamed the script to download the ligand data in sdf: fetch_sdf_ligands_pdb_cif_res. Now it extract the ligand sctructure from its PDB cif file and save to separated cif files. Updated description and documentation accoardling

--- np3_LigPCDS/src/fetch_sdf_ligands_pdb_cif_res.py
+++ np3_LigPCDS/src/fetch_sdf_ligands_pdb_cif_res.py
@@ -1,4 +1,3 @@
-from Bio.PDB import *
 import urllib.request
 import pandas as pd
 import sys
@@ -6,6 +5,7 @@ from pathlib import Path
 from rdkit.Chem import SDMolSupplier
 from tqdm import tqdm
 from rdkit import RDLogger
+from Bio.PDB import MMCIFParser, MMCIFIO, Select
 
 # Disable the warning message - prevent warning molecule is tagged as 2D, here its always 3D
 RDLogger.DisableLog('rdApp.*')
@@ -57,15 +57,15 @@ def fetch_sdf_ligands(db_path, ligs_report_file, n, i_start=0):
     if n <= i_start and not n == 0:
         sys.exit("The provided stop row is not greater than the start row. Wrong range.")
 
-    # check if folder exists, if not create it
+    # check if data folder exists, if not create it
     data_folder = Path(db_path)
     if not data_folder.exists() or not data_folder.is_dir():
         sys.exit("The provided data folder do not exists.")
-    # create the directories ligands and pdb_no_lig if they do not exists yet
+    # create the directory 'ligands' if it does not exist yet
     Path(data_folder / 'ligands').mkdir(parents=True, exist_ok=True)
-    #Path(data_folder / 'pdb_no_lig').mkdir(parents=True, exist_ok=True)
-    # create the PDB parser, QUIET set to True to suppress warnings
-    parser = PDBParser(PERMISSIVE=1, QUIET=True)
+    # create the CIF parser, QUIET set to True to suppress warnings
+    parser = MMCIFParser(QUIET=True)
+
     # read csv with ligands list to retrieve
     ligs_retrieve_list = pd.read_csv(ligs_report_file, na_values=['null', 'N/A'], keep_default_na=False)
     # get the list of pdb ids in lower case, to be used to retrieve the ligands data
@@ -87,9 +87,9 @@ def fetch_sdf_ligands(db_path, ligs_report_file, n, i_start=0):
             print("No MTZ found. Skipping structure.")
             continue
 
-        # get the list of already downloaded ligands - check their .pdb file
+        # get the list of already downloaded ligands - check their .cif files
         ligs_present = list(dict.fromkeys([lig_pdb.name.split('_')[1] for lig_pdb in
-                                         data_folder.glob('ligands/'+structure_id+'_*.pdb')]))
+                                         data_folder.glob('ligands/'+structure_id+'_*.cif')]))
 
         # filter from the ligands report list the ones that appear in this pdbid
         ligs_pdbid_list = ligs_retrieve_list.loc[ligs_retrieve_list.PDBID == structure_id, :]
@@ -104,18 +104,20 @@ def fetch_sdf_ligands(db_path, ligs_report_file, n, i_start=0):
         ligs_pdbid_list.loc[:, "LigandIDRes"] = ['H_{:>3s}'.format(ligID) for ligID in ligs_pdbid_list.LigandID]
 
         # parse structure to get access to the residues and ligands
-        file_pdb = (data_folder / 'pdb' / str('pdb' + structure_id + '.ent'))
+        #file_pdb = (data_folder / 'pdb' / str('pdb' + structure_id + '.ent'))
+        file_pdb_cif = (data_folder / 'pdb_cif' / str(structure_id + '.cif'))
         # check if the pdb files could be downloaded
-        if not file_pdb.exists():
-            print("No PDB found. Skipping structure.")
+        if not file_pdb_cif.exists():
+            print("No PDB CIF found. Skipping structure.")
             continue
         try:
-            structure = parser.get_structure(structure_id, file_pdb)
+            structure = parser.get_structure(structure_id, file_pdb_cif)
         except:
-            print("ERROR PDB parser")
+            print("ERROR PDB CIF parser")
             continue
 
-        io = PDBIO()
+        # create the CIF IO to extract the ligands from the structure and save to separated .cif files
+        io = MMCIFIO()
         io.set_structure(structure)
         models = list(structure.get_models())
         if (len(models) == 0):
@@ -123,7 +125,7 @@ def fetch_sdf_ligands(db_path, ligs_report_file, n, i_start=0):
             continue
 
         # iterate over the structure residues in all chains (in AuthAsymID) to locate the desired ligands
-        print("** Start downloading ligands SDF data, extracting .pdb and parsing the data **")
+        print("** Start downloading ligands SDF data, extracting .cif and parsing the data **")
         for residue in models[0].get_residues():
             if residue.get_id()[0] in ligs_pdbid_list.LigandIDRes.values or residue.get_id()[0] == 'W':
                 id = list(residue.get_id())
@@ -132,9 +134,8 @@ def fetch_sdf_ligands(db_path, ligs_report_file, n, i_start=0):
                 #print(str(id))
                 # do not remove the prefix H_ from the water residue and skip its sdf retrieval
                 if id[0] == 'W':
-                    # water ligs, store all water's residue in a single pdb
-                    # save a pdb with all the water coordinates
-                    io.save((data_folder / 'ligands' / str(structure_id + "_W_lig.pdb")).as_posix(),
+                    # water ligs, store all water's residue in a single cif file with all the water coordinates
+                    io.save((data_folder / 'ligands' / str(structure_id + "_W_lig.cif")).as_posix(),
                             ResSelect(residue.get_id(), id[2]))
                 else:  # other ligs
                     id_name = '_'.join(id)[2:].strip()
@@ -150,16 +151,11 @@ def fetch_sdf_ligands(db_path, ligs_report_file, n, i_start=0):
                         elif mol_res[0].GetNumAtoms() == 0:
                             print('ERROR no atoms in sdf ' + file_sdf.name + "\n")
                             file_sdf.unlink(missing_ok=True)
-                        else:
-                            # if sdf is present
-                            # save the ligand in .pdb and the entry pdb without the current ligand
-                            if not (data_folder / 'ligands' / str(structure_id + "_" + id_name + "_lig.pdb")).exists():
-                                # save a pdb without the ligand residue
-                                #io.save((data_folder / 'pdb_no_lig' / str(structure_id + "_" + id_name + ".pdb")).as_posix(),
-                                #        ResSkip(residue.get_id(), id[2]))
-                                # save a pdb of the ligand residue, the ligand coordinates
-                                io.save((data_folder / 'ligands' / str(structure_id + "_" + id_name + "_lig.pdb")).as_posix(),
-                                        ResSelect(residue.get_id(), id[2]))
+                    # if sdf is present - thus minimal valid
+                    # save a cif with the ligand residue, containing the ligand coordinates
+                    if file_sdf.exists() and not (data_folder / 'ligands' / str(structure_id + "_" + id_name + "_lig.cif")).exists():
+                        io.save((data_folder / 'ligands' / str(structure_id + "_" + id_name + "_lig.cif")).as_posix(),
+                                ResSelect(residue.get_id(), id[2]))
 
 
 if __name__ == "__main__":
@@ -174,9 +170,9 @@ if __name__ == "__main__":
             n = int(sys.argv[4])
     else:
         sys.exit("Wrong number of arguments. At least two arguments must be supplied in order to retrieve the ligands "
-                 ".sdf data, extract the ligands .pdb and to create the structures .pdb without each ligand coordinates: \n"
+                 ".sdf data and extract the ligands .cif from the structures .cif: \n"
                  "  1. The path to the data folder where the data will be stored. One folder will be created: 'ligands' to store "
-                 "te .sdf and the .pdb files of the ligands;\n"
+                 "te .sdf and the .cif files of the ligands;\n"
                  "  2. The path to the CSV file containing the ligands report list to be retrieved and the PDB ids in "
                  "which they appear. Mandatory columns = 'EntryID', 'LigandID', and 'AuthAsymID'. The last "
                  "column must contain the chains where the respective LigandID is located in the respective EntryID;\n"
