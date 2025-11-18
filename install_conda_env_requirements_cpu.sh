785ae7d1e57e3b8f39aa5fd55ed65160d8cb332f
crisfbazz <crisfbazz@gmail.com>
2025-11-18 15:25:39 -0300

    np3_lig_install_scripts: updated installation scripts with gcc 9, unified requerements file, correct cuda chanel with fixed version and cloning ME repo into lib folder for installation

--- install_conda_env_requirements_cpu.sh
+++ install_conda_env_requirements_cpu.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 ### Install python 3.9 using ANACONDA
 ###
-export CXX=g++-11; 
+export CXX=g++-9;
 conda create -n np3_lig python=3.9 -y
 conda activate np3_lig
 # explicity set gcc equals 11 in conda env
-conda install -c conda-forge gcc=11.2.0 gxx=11.2.0 -y
+conda install -c conda-forge gcc=9 gxx=9 -y
 conda install openblas-devel -c anaconda -y
 conda install r-base=4.4.0 r-readr r-dplyr -c conda-forge -y
 # open3D dependency to draw geometrics
@@ -12,13 +13,17 @@ conda install -c conda-forge libstdcxx-ng=13.2 -y
 ## install R anticlust package - not present in conda                            
 Rscript -e 'install.packages("anticlust",repos = "http://cran.us.r-project.org")' 
 # install pip requerements
-pip install -r requirements_np3_ligand_cpu.txt --extra-index-url https://download.pytorch.org/whl/cpu
+pip install -r requirements_np3_ligand.txt --extra-index-url https://download.pytorch.org/whl/cpu
 
 # install Minkowski Engine
-export CXX=g++-11; # set this if you want to use a different C++ compiler
-if [ ! -d 'MinkowskiEngine' ]; then 
+export CXX=g++-9; # set this if you want to use a different C++ compiler
+if [ ! -d 'lib/MinkowskiEngine' ]; then
+  mkdir lib && cd lib
   git clone https://github.com/NVIDIA/MinkowskiEngine.git
+  cd MinkowskiEngine
+else
+  cd lib/MinkowskiEngine
 fi
-cd MinkowskiEngine
 export MAX_JOBS=2; # parallel compilation - prevent to much CPU assignment and process killed
 python setup.py install --blas_include_dirs=${CONDA_PREFIX}/include:/usr/include/ --blas=openblas --cpu_only
+cd ../..
--- install_conda_env_requirements_cuda11.8.sh
+++ install_conda_env_requirements_cuda11.8.sh
@@ -5,7 +5,7 @@ export CXX=g++-9; # set this if you want to use a different C++ compiler
 conda create -n np3_lig python=3.9 -y
 conda activate np3_lig
 # explicity set gcc equals 11 in conda env
-conda install -c conda-forge gcc=9.5 gxx=9.5 -y
+conda install -c conda-forge gcc=9 gxx=9 -y
 conda install openblas-devel -c anaconda -y
 conda install r-base=4.4.0 r-readr r-dplyr -c conda-forge -y
 # open3D dependency to draw geometrics
@@ -13,19 +13,23 @@ conda install -c conda-forge libstdcxx-ng=13.2 -y
 ## install R anticlust package - not present in conda                            
 Rscript -e 'install.packages("anticlust",repos = "http://cran.us.r-project.org")'
 ## For CUDA capability also install cuda and cudatoolkit
-conda install nvidia/label/cuda-11.8.0::cuda -y
-conda install nvidia/label/cuda-11.8.0::cuda-toolkit -y  
+conda install -c "nvidia/label/cuda-11.8.0" cuda cuda-toolkit -y
+
 # install pip requerements
-pip install -r requirements_np3_ligand_cuda11.8.txt --extra-index-url https://download.pytorch.org/whl/cu118
+pip install -r requirements_np3_ligand.txt --extra-index-url https://download.pytorch.org/whl/cu118
 
 # install Minkowski Engine
-export CXX=g++-11; # set this if you want to use a different C++ compiler
+export CXX=g++-9; # set this if you want to use a different C++ compiler
 export CUDA_HOME=$(dirname $(dirname $(which nvcc))); # or select the correct cuda version on your system.
 export LD_LIBRARY_PATH=$CUDA_HOME/lib:$LD_LIBRARY_PATH
 export PATH=$CUDA_HOME/bin:$PATH 
-if [ ! -d 'MinkowskiEngine' ]; then 
+if [ ! -d 'lib/MinkowskiEngine' ]; then
+  mkdir lib && cd lib
   git clone https://github.com/NVIDIA/MinkowskiEngine.git
-fi 
-cd MinkowskiEngine
+  cd MinkowskiEngine
+else
+  cd lib/MinkowskiEngine
+fi
 export MAX_JOBS=2; # parallel compilation - prevent to much CPU assignment and process killed
 python setup.py install --blas_include_dirs=${CONDA_PREFIX}/include --blas=openblas --force_cuda
+cd ../..
